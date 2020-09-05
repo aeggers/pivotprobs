48fe26c0554b252348968cca2d9b20beb71c625c
Andy Eggers <aeggers@gmail.com>
2020-09-05 08:54:14 +0100

    checks and features for general approach

---
+++ R/event_list_functions.R
@@ -0,0 +1,132 @@
+## functions for recovering events in each system
+
+plurality_event_list <- function(n = 1000, k = 4, max_pivot_event_degree = 2){
+
+  if(!max_pivot_event_degree %in% 1:2){
+    stop("max_pivot_event_degree needs to be either 1 or 2. The event_probabilities_from_event_list() function only permutes three candidates, so we cannot handle pivot events of degree 3 or higher, e.g. four-way ties.")
+  }
+
+  el <- list()
+
+  win_conditions <- cbind(1, -diag(k-1), 1/n) # i beats j, k, ...
+  win_P <- matrix(0, nrow = k, ncol = k)
+  win_P[1, ] <- 1
+
+  el[["i_"]] <- list(
+    conditions = win_conditions,
+    rows_to_alter = c(),
+    scaling_factor = 1,
+    P = win_P
+  )
+
+  this_P <- win_P
+  this_P[1:2, 2] <- c(0,1)
+
+  el[["i_j"]] <- list(
+    conditions = el[["i_"]]$conditions,
+    rows_to_alter = c(1),                      # i barely beats j
+    scaling_factor = 1/(sqrt(2)*n),
+    P = this_P,
+    adjacent_events = "j_i"
+  )
+
+  if(max_pivot_event_degree == 2){
+
+    this_P <- win_P
+    this_P[1:2, 2] <- c(0,1)
+    this_P[1:3, 3] <- c(0,0,1)
+
+    el[["i_jk"]] <- list(
+      conditions = el[["i_"]]$conditions,
+      rows_to_alter = c(1,2),                    # i barely beats j and k
+      scaling_factor = (1/(sqrt(2)*n))^2,
+      P = this_P,
+      adjacent_events = c("j_ik", "k_ij") # note "k_ij" and "k_ji" both get stored as "k_ij"
+    )
+  }
+
+  el
+}
+
+positional_event_list <- function(n = 1000, s = .5){
+
+  el <- list()
+
+  el[["i_"]] <- list(
+    conditions = rbind(c(1-s, 1, s-1, -1, s, -s, 1/n),  # i scores above j
+                     c(1, 1-s, s, -s, s-1, -1, 1/n)),   # i scores above k
+    rows_to_alter = c(),
+    scaling_factor = 1,
+    P = rbind(rep(1, 6), 0, 0, 0)
+    )
+
+  el[["i_j"]] <- list(
+    conditions = el[["i_"]]$conditions,
+    rows_to_alter = c(1),                               # i barely scores above j
+    scaling_factor = 1/(2*sqrt(1 - s + s^2)*n),
+    P = rbind(c(1,1,s,0,1,1-s),
+            c(0,0,1-s,1,1,s),
+            0),
+    adjacent_events = "j_i"
+    )
+
+  # not doing second-degree pivot events (a point), but could add -- would need to get scaling factor
+
+  el
+}
+
+irv_event_list <- function(n = 1000){
+
+  # eventually good to make the first round positional
+
+  irv_events <- list()
+
+  irv_events[["i__j"]] <- list(
+    conditions = rbind(c(1, 1,0,0,-1,-1, 1/n),  # i beats k in first round
+                       c(0,0,1,1,-1,-1,1/n),    # so does j
+                       c(1,1,-1,-1,1,-1, 1/n)), # i beats j in second round
+    rows_to_alter = c(),
+    scaling_factor = 1,
+    P = rbind(rep(1, 6), 0, 0, 0)
+  )
+
+  irv_events[["i_j"]] <- list(
+    conditions = irv_events[["i__j"]]$conditions,
+    rows_to_alter = c(3),                        # i barely beats j in second round
+    scaling_factor = 1/(2*n), # not sure about this -- will have to find out
+    P = rbind(c(1,1,0,0,1,0), c(0,0,1,1,0,1), 0),
+    adjacent_events = "j_i")
+
+  irv_events[["i_j|ij"]] <- list(
+    conditions = rbind(c(-1, -1,0,0,1,1, 1/n),    # k beats i in first round
+                       c(0,0,-1,-1,1,1, 1/n),      # k beats j in first round
+                       c(1,1,-1,-1,0,0, 1/n),     # i beats j in first round
+                       c(1,1,1,-1,-1,-1, 1/n),    # i beats k in second round
+                       c(1,-1,1,1,-1,-1, 1/n)),   # j beats k in second round
+    rows_to_alter = c(3),                         # i barely beats j in first round
+    scaling_factor = 1/(2*n), # not sure about this -- will have to find out
+    P = rbind(c(1,1,0,0,1,1), c(0,0,1,1,0,0), 0),
+    adjacent_events = "j_i|ji")
+
+  i_j_ik_conditions <- irv_events[["i_j|ij"]]$conditions
+  i_j_ik_conditions[5,] <- -i_j_ik_conditions[5,]
+
+  irv_events[["i_j|ik"]] <- list(
+    conditions = i_j_ik_conditions,
+    rows_to_alter = c(3),
+    scaling_factor = 1/(2*n), # not sure about this -- will have to find out
+    P = rbind(c(1,1,1,1,0,0), 0, c(0,0,0,0,1,1)),
+    adjacent_events = "j_i|ki")
+
+  i_j_kj_conditions <- irv_events[["i_j|ij"]]$conditions
+  i_j_kj_conditions[4,] <- -i_j_ik_conditions[4,]
+
+  irv_events[["i_j|kj"]] <- list(
+    conditions = i_j_kj_conditions,
+    rows_to_alter = c(3),
+    scaling_factor = 1/(2*n), # not sure about this -- will have to find out
+    P = rbind(0, c(0,0,1,1,0,0), c(1,1,0,0,1,1)),
+    adjacent_events = "j_i|jk")
+
+  irv_events
+}
--- R/event_probabilities_from_event_list.R
+++ R/event_probabilities_from_event_list.R
@@ -1,6 +1,6 @@
 # I think this is the way forward.
 
-event_probabilities_from_event_list <- function(event_list = NULL, alpha = NULL, mu = NULL, precision = NULL, sigma = NULL, cand_names = NULL, ordinal = T, drop_dimension = F, merge_adjacent_pivot_events = F, skip_non_pivot_events = F, store_time = T, ...){
+event_probabilities_from_event_list <- function(event_list = NULL, alpha = NULL, mu = NULL, precision = NULL, sigma = NULL, sims = NULL, draw_sims = F, num_sims = 100000, cand_names = NULL, ordinal = T, drop_dimension = F, merge_adjacent_pivot_events = F, skip_non_pivot_events = F, store_time = T, ...){
 
   if(is.null(event_list)){stop("You have to pass an event_list.")}
 
@@ -10,38 +10,55 @@ event_probabilities_from_event_list <- function(event_list = NULL, alpha = NULL,
   # start the clock
   time_start <- Sys.time()
 
-  # figure out the distribution from the parameters
-  if(!is.null(alpha) | (!is.null(mu) & !is.null(precision)) & is.null(sigma)){
-    # this is Dirichlet
-    distribution = "dirichlet"
-    if(is.null(alpha)){alpha <- mu*precision}
-    mu <- alpha/sum(alpha)
-  }else if(!is.null(mu) & !is.null(sigma)){
-    # this is logistic normal
-    distribution = "logisticnormal"
+  if(is.null(sims)){
+    # figure out the distribution from the parameters
+    if(!is.null(alpha) | (!is.null(mu) & !is.null(precision)) & is.null(sigma)){
+      # this is Dirichlet
+      distribution <- "dirichlet"
+      if(is.null(alpha)){alpha <- mu*precision}
+      mu <- alpha/sum(alpha)
+    }else if(!is.null(mu) & !is.null(sigma)){
+      # this is logistic normal
+      distribution <- "logisticnormal"
+    }else{
+      stop("Please pass parameters that allow me to determine the intended distribution over election outcomes.")
+    }
   }else{
-    stop("Please pass parameters that allow me to determine the intended distribution over election outcomes.")
+    distribution <-  "sims"
   }
 
-  # set limits based on arguments
+  # get 1/n from the conditions -- is this risky?
+  conditions_1 <- event_list[[1]]$conditions
+  base_limit <- conditions_1[1, ncol(conditions_1)]
+  if(class(base_limit) != "numeric"){
+    stop(paste0("Was expecting last column of first row of first condition to be 1/n; instead it is ", base_limit, "."))
+  }
+  if(base_limit <= 0){
+    stop("I assumed that the last column of the first condition on the first event is 1/n, but it is", base_limit, ".")
+  }
+
+  # set limits (width of integration region) based on arguments
   if(merge_adjacent_pivot_events & drop_dimension){
-    limits <- c(1/(2*n), 1/(2*n))
+    limits <- c(0, 0) # only first limit used -- a line at the boundary
   } else if(merge_adjacent_pivot_events){
-    limits <- c(-1/(2*n), 1/(2*n))
+    limits <- c(-base_limit/2, base_limit/2)  # a thin strip straddling the boundary
   } else if(drop_dimension){
-    limits <- c(0, 0)
+    limits <- rep(base_limit/2, 2)   # only first limit used -- a line just short of the boundary
   } else{
-    limits <- c(0, 1/n)
+    limits <- c(0, base_limit)     # a thin strip short of the boundary
   }
 
   if(ordinal){
     if(is.null(cand_names)){
       cand_names <- letters[1:3]
     }
-    if(length(mu) != 6 | length(cand_names) != 3){
-      stop("For ordinal methods I expect 3 candidates and parameters indicating 6 ballots. You either provided more than 3 candidate names or parameters indicating more or fewer than 6 ballots.")
+    if(length(mu) != 6){
+      stop("For ordinal methods I expect parameters for 6 ballot types. You provided parameters indicating more or fewer than 6 ballot types.")
     }
-  }else{
+    if(length(cand_names) != 3){
+      stop("For ordinal methods I expect 3 candidates. You provided more or fewer than that.")
+    }
+  }else{  # single-ballot syste
     if(is.null(cand_names)){
       cand_names <- letters[1:length(mu)]
     }
@@ -71,29 +88,58 @@ event_probabilities_from_event_list <- function(event_list = NULL, alpha = NULL,
       this_P <- event_list[[names(event_list)[el_index]]]$P
 
       # we use the P matrix to determine if this is a pivot event
-      if(skip_non_pivot_events & (this_P %>% apply(1, mean) %>% max() == 1)){next}
+      non_pivot_event <- this_P %>% apply(1, mean) %>% max() == 1
+      if(skip_non_pivot_events & non_pivot_event){next}
 
       # start the clock
       this_time_start <- Sys.time()
 
       # we need to convert e.g. i_j to c_a
       generic_event_name <- names(event_list)[el_index]
-      specific_event_name <- generic_event_name %>%
-        str_replace_all("i", candidate_permutation_mat[p_row, "i"]) %>%
-        str_replace_all("j", candidate_permutation_mat[p_row,"j"]) %>%
-        str_replace_all("k", candidate_permutation_mat[p_row, "k"])
 
-      # a_bc and a_cb mean the same thing. so we'll store both as a_bc
-      # but we don't want to change a_b|cb into something else
-      if(str_detect(specific_event_name, "_")){
-        split_name <- specific_event_name %>% str_replace("\\|.+", "") %>% str_split("") %>% unlist()
-        if(length(split_name) > 3){
-          specific_event_name <- paste0(paste(split_name[1:2], collapse = ""), paste(sort(split_name[3:length(split_name)]), collapse = ""))
-        }
+      # two functions I use a couple times here. uses local variables in definition.
+      turn_generic_to_specific <- function(generic_event_name){
+        generic_event_name %>%
+          str_replace_all("i", candidate_permutation_mat[p_row, "i"]) %>%
+          str_replace_all("j", candidate_permutation_mat[p_row,"j"]) %>%
+          str_replace_all("k", candidate_permutation_mat[p_row, "k"])
       }
 
-      # now we check if we've already done this exact one
-      if(specific_event_name %in% names(out)){next}
+      # turn generic event name (e.g. i_j) into name that corresponds to this permutation (e.g. c_a)
+      specific_event_name <- generic_event_name %>%
+        turn_generic_to_specific() %>%    # i_j => c_a
+        regularize_candidate_order_after_underscore()  # c_ba => c_ab, but c_ba|cb stays same (if we ever have such an event)
+
+      # we may have already done this one (after regularization)
+      if(specific_event_name %in% names(out)){
+        cat(generic_event_name, " -> ", specific_event_name, " already entered -- skipping.\n")
+        next
+      }
+
+      # check whether we have already stored a pivot event adjacent to this one
+      if(merge_adjacent_pivot_events & ! non_pivot_event){
+        generic_adjacent_event_names <- event_list[[generic_event_name]]$adjacent_events
+        if(!is.null(generic_adjacent_event_names)){
+          specific_adjacent_event_names <- generic_adjacent_event_names %>%
+            map_chr(turn_generic_to_specific) %>%
+            map_chr(regularize_candidate_order_after_underscore)
+          cat(generic_event_name, " can be merged with ", generic_adjacent_event_names %>% paste(collapse = ", "), ". After transformation, looking to merge ", specific_event_name, " with ", specific_adjacent_event_names %>% paste(collapse = ""), ".\n", sep = "")
+          cat("names(out) is ", names(out) %>% paste(collapse = ", "), ".\n")
+          in_out <- which(specific_adjacent_event_names %in% names(out))[1]
+          if(!is.na(in_out)){
+            cat("Found one! ", specific_adjacent_event_names[in_out], ".\n", sep = "")
+            # store existing result from this adjacent event
+            out[[specific_event_name]] <- out[[specific_adjacent_event_names[in_out]]]
+            # but not the P matrix -- that should be from this event
+            out[[specific_event_name]]$W_mat <- this_P[cand_order, ballot_order]
+            # store time
+            time_diff <- Sys.time() - this_time_start
+            units(time_diff) <- "secs"
+            out[[specific_event_name]]$seconds_elapsed <- as.double(time_diff)
+            next  # on to the next event
+          }
+        }
+      }
 
       cat(generic_event_name, " -> ", specific_event_name, ".\n", sep = "")
 
@@ -145,3 +191,14 @@ event_probabilities_from_event_list <- function(event_list = NULL, alpha = NULL,
   out
 
 }
+
+regularize_candidate_order_after_underscore <- function(specific_event_name){
+  if(str_detect(specific_event_name, "_") & !str_detect(specific_event_name, "\\|")){
+    split_name <- specific_event_name %>% str_split("") %>% unlist()
+    if(length(split_name) > 3){
+      specific_event_name <- paste0(paste(split_name[1:2], collapse = ""), paste(sort(split_name[3:length(split_name)]), collapse = ""))
+    }
+  }
+  specific_event_name
+}
+
