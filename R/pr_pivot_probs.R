9a651c9668abbd185d66a07dcc68c7aa81cde8ec
Andy Eggers <aeggers@gmail.com>
2021-06-17 14:55:59 +0100

    cleaned up pr_pivot_probs, added magrittr as dependency

--- R/pr_pivot_probs.R
+++ R/pr_pivot_probs.R
@@ -40,20 +40,20 @@ dhondt_pivot_probs_dirichlet <- function(alpha_vec, M, increments = 50, cand_nam
 
 divisor_gridpoints_ab <- function(M, divisors = "dhondt"){
 
-  if(divisors == "dhondt"){
+  if(length(divisors) > 1){
+    divs <- divisors
+  }else if(divisors == "dhondt"){
     divs <- 1:(M+1)
   }else if(divisors == "sainte-lague"){
     divs <- 1 + 2*(0:M)
   }else if(divisors == "koudeika"){
     divs <- c(sqrt(2), 2:(M+1))
-  }else if(length(divisors) > 1){
-    divs <- divisors
+  }else{
+    stop("We don't recognize that kind of divisor.")
   }
 
   # for a-b ties where c is getting 0 seats, we need a divisor that corresponds to getting 0 seats. we set this at 1e-10.
   divs <- c(1e-10, divs)
-
-  cat(length(divs))
 
   # "pot_seats" refers to the number of seats the candidate could win given one more vote; a_pot_seats = 1 and b_pot_seats = 1 means one of them is going to win one seat, but not both
   expand_grid(a_pot_seats = 1:M, b_pot_seats = 1:M, c_boost = c(0,1)) %>%
@@ -114,114 +114,51 @@ divisor_gridpoints <- function(M, divisors = "dhondt"){
 
 }
 
-divisor_gridpoints(M = 2, divisors = "sainte-lague") %>%
-  mutate(a = a + .5*b,
-         b = sqrt(3/4)*b) %>%
-  ggplot(aes(x = a, y = b)) +
-  geom_line(aes(group = event, col = event_class)) +
-  expand_limits(x = c(0,1), y = c(0,1)) +
-  coord_fixed() +
-  theme_void()
 
 
-
+# # illustration
+# divisor_gridpoints(M = 4, divisors = "dhondt") %>%
+#   mutate(a = a + .5*b,
+#          b = sqrt(3/4)*b) %>%
+#   ggplot(aes(x = a, y = b)) +
+#   geom_line(aes(group = event, col = event_class)) +
+#   expand_limits(x = c(0,1), y = c(0,1)) +
+#   coord_fixed() +
+#   theme_void()
 #
-#   # we investigate ties between x and y
-#   tibble(x_seats_high = 1:M) %>%
-#     expand_grid(z_seats = 0:M) %>%
-#     mutate(y_seats_low = M - x_seats_high - z_seats) %>%
-#     filter(y_seats_low >= 0) -> low_high
+# divisor_gridpoints(M = 4, divisors = "sainte-lague") %>%
+#   mutate(a = a + .5*b,
+#          b = sqrt(3/4)*b) %>%
+#   ggplot(aes(x = a, y = b)) +
+#   geom_line(aes(group = event, col = event_class)) +
+#   expand_limits(x = c(0,1), y = c(0,1)) +
+#   coord_fixed() +
+#   theme_void()
 #
-#   low_high %>%
-#     mutate(xstar_low = 1/(1 + (divs[y_seats_low] + divs[z_seats + 1])/divs[x_seats_high]),
-#            xstar_high = 1/(1 + (divs[y_seats_low] + divs[z_seats])/divs[x_seats_high]),
-#            x_low = xstar_low,
-#            y_low = xstar_low*divs[y_seats_low]/divs[x_seats_high],
-#            z_low = xstar_low*divs[z_seats + 1]/divs[x_seats_high],
-#            x_high = xstar_high,
-#            y_high = xstar_high*divs[y_seats_low]/divs[x_seats_high],
-#            z_high = xstar_high*divs[z_seats]/divs[x_seats_high]) -> tmp
+# divisor_gridpoints(M = 4, divisors = "koudeika") %>%
+#   mutate(a = a + .5*b,
+#          b = sqrt(3/4)*b) %>%
+#   ggplot(aes(x = a, y = b)) +
+#   geom_line(aes(group = event, col = event_class)) +
+#   expand_limits(x = c(0,1), y = c(0,1)) +
+#   coord_fixed() +
+#   theme_void()
 #
-#
-#   # we get the endpoints of the locus where
-#   expand_grid(a_pot_seats = 1:M, b_pot_seats = 1:M, M_boost = c(1,2)) %>%
-#     filter(a_pot_seats + b_pot_seats <= M + 1) %>%
-#     mutate(
-#       a = a_pot_seats/(M + M_boost),
-#       b = a*b_pot_seats/a_pot_seats,
-#       c = 1 - a - b,
-#       c_seats = M - a_pot_seats - b_pot_seats + 1
-#     ) %>%
-#     select(-M_boost)
-# }
+# # c(1, 4, 7, 11))
+# divisor_gridpoints(M = 13, divisors = seq(1, 200, by = 1)) %>%
+#   mutate(a = a + .5*b,
+#          b = sqrt(3/4)*b) %>%
+#   ggplot(aes(x = a, y = b)) +
+#   geom_line(aes(group = event, col = event_class)) +
+#   expand_limits(x = c(0,1), y = c(0,1)) +
+#   coord_fixed() +
+#   theme_void()
 
 
-dhondt_gridpoints_ab <- function(M){
 
-  # we get the endpoints of the locus where
-  expand_grid(a_pot_seats = 1:M, b_pot_seats = 1:M, M_boost = c(1,2)) %>%
-    filter(a_pot_seats + b_pot_seats <= M + 1) %>%
-    mutate(
-      a = a_pot_seats/(M + M_boost),
-      b = a*b_pot_seats/a_pot_seats,
-      c = 1 - a - b,
-      c_seats = M - a_pot_seats - b_pot_seats + 1
-    ) %>%
-    select(-M_boost)
-}
-
-dhondt_gridpoints <- function(M){
-
-  dgp <- dhondt_gridpoints_ab(M) %>%
-    mutate(a_low = a_pot_seats - 1,
-           b_low = b_pot_seats - 1,
-           a_high = a_pot_seats,
-           b_high = b_pot_seats) %>%
-    select(a_low, a_high, b_low, b_high, c_lowhigh = c_seats, a, b, c)
-
-  bind_rows(
-    # ab
-    dgp %>%
-      mutate(
-        event_1 = pmap_chr(select(., a_low, b_high, c_lowhigh), str_c, sep = "."),
-        event_2 = pmap_chr(select(., a_high, b_low, c_lowhigh), str_c, sep = "."),
-        event_class = "ab"
-      ),
-    # ac
-    dgp %>%
-      mutate(
-        event_1 = pmap_chr(select(., a_low, c_lowhigh, b_high), str_c, sep = "."),
-        event_2 = pmap_chr(select(., a_high, c_lowhigh, b_low), str_c, sep = "."),
-        event_class = "ac"
-      ) %>%
-      rename(c = b,
-             b = c),
-    # bc
-    dgp %>%
-      mutate(
-        event_1 = pmap_chr(select(., c_lowhigh, a_low, b_high), str_c, sep = "."),
-        event_2 = pmap_chr(select(., c_lowhigh, a_high, b_low), str_c, sep = "."),
-        event_class = "bc"
-      ) %>%
-      rename(
-        a = c,
-        b = a,
-        c = b
-      )
-  ) %>%
-    mutate(event = paste0(event_1, "_", event_2)) %>%
-    select(event, event_class, a, b, c)
-
-}
-
-dhondt_gridpoints(M = 2) %>%
-  mutate(a = a + .5*b,
-         b = sqrt(3/4)*b) %>%
-  ggplot(aes(x = a, y = b)) +
-  geom_line(aes(group = event, col = event_class)) +
-  expand_limits(x = c(0,1), y = c(0,1)) +
-  coord_fixed() +
-  theme_void()
+# Next to do:
+# add rows for the non-pivot events for both plotting and integration.
+# add that to event_list_functions so that we can get the P matrix including non pivot events.
 
 outcome_names_from_M <- function(M){
   expand_grid(a = 0:M, b = 0:M) %>%
